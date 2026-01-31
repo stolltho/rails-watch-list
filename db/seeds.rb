1597db5ab53e3e611ab7e4531c2a528642955dbc
Thomas Stoll <stolltho@gmail.com>
2026-01-31 12:16:28 +1000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,49 @@
 #   end
 
 puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
 Movie.destroy_all
 
 puts "Creating movies..."
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+wonder_woman = Movie.create!(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9
+)
 
-puts "Finished! Created #{Movie.all.count} movies in the database"
+shawshank = Movie.create!(
+  title: "The Shawshank Redemption",
+  overview: "Framed in the 1940s for double murder...",
+  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  rating: 8.7
+)
+
+titanic = Movie.create!(
+  title: "Titanic",
+  overview: "101-year-old Rose DeWitt Bukater tells the story...",
+  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+  rating: 7.9
+)
+
+oceans = Movie.create!(
+  title: "Ocean's Eight",
+  overview: "Debbie Ocean, a criminal mastermind...",
+  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+  rating: 7.0
+)
+
+puts "Creating lists..."
+drama = List.create!(name: "Drama")
+favs = List.create!(name: "All time favs")
+girl_power = List.create!(name: "Girl Power")
+
+puts "Creating bookmarks..."
+Bookmark.create!(comment: "Recommended by John", movie: titanic, list: favs)
+Bookmark.create!(comment: "Superhero movie revisited in 2020", movie: wonder_woman, list: girl_power)
+Bookmark.create!(comment: "Spielberg's masterly Oscar-winning drama", movie: shawshank, list: drama)
+Bookmark.create!(comment: "2020 release", movie: oceans, list: favs)
+Bookmark.create!(comment: "Based on Stephen King's novel", movie: shawshank, list: girl_power)
+
+puts "Finished! Created #{Movie.count} movies, #{List.count} lists and #{Bookmark.count} bookmarks."
