2ffaea8b064b61c0ce06bbb18430533bcc06e080
Thomas Stoll <stolltho@gmail.com>
2026-01-31 14:26:40 +1000

    basic setup routes-controller-view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,34 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
   def new
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    # @bookmark.list_id = @list.id
+    @bookmark.save
+    redirect_to list_path(@list)
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@boomkark.list), status: :see_other
   end
+
+    private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,25 @@
 class ListsController < ApplicationController
   before_action :set_list, only: %i[show]
+  
   def index
     @lists = List.all
   end
 
   def show
+    @bookmarks = @list.bookmarks
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
