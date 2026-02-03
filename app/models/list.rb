a124eb55eaaae5544691ed7fd403f2a9b1385699
Thomas Stoll <stolltho@gmail.com>
2026-02-03 19:34:08 +1000

    add active-storage

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,7 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  has_one_attached :photo
+
   validates :name, presence: true, uniqueness: true
 end
